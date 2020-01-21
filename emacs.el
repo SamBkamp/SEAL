b79f43371f0bb01d3574f13e3f76994f8e49cfe9
Sam Bonnekamp <samuel@bonnekamp.net>
2020-01-21 18:49:56 +0800

    added autofill int main()

--- emacs.el
+++ emacs.el
@@ -1,9 +1,18 @@
+(require 'package)
+
+(package-initialize)
+
+(add-to-list 'package-archives
+'("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deeper-blue))))
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(package-selected-packages (quote (phps-mode magit-gitflow magit ##))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -11,11 +20,22 @@
  ;; If there is more than one, they won't work right.
  )
 
+(defun bkamp/format-line()
+  (kill-whole-line) 
+  (insert "int main(int argc, char* argv[]){\n\n}")
+  (backward-char 2)
+  (indent-for-tab-command)
+  )
+
 (defun int-main ()
   (interactive)
   (if (string= (thing-at-point 'line) "main")
-      (insert "int main(int argc, char* argv[]){\n}")
-    (indent-for-tab-command)))
+      (bkamp/format-line)
+    (if (string= (thing-at-point 'line) "main\n")
+	(bkamp/format-line)
+      (indent-for-tab-command))
+    )
+  )
 
 (defun closing-brace ()
   (interactive)
@@ -38,3 +58,48 @@
 
 (add-hook 'c-initialization-hook
   (lambda () (define-key c-mode-base-map (kbd "TAB") 'int-main)))
+
+
+(defun rofrol/indent-region(numSpaces)
+    (progn 
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
+
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards            
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun rofrol/indent-lines(&optional N)
+    (interactive "p")
+    (indent-rigidly (line-beginning-position)
+                    (line-end-position)
+                    (* (or N 1) tab-width)))
+
+(defun rofrol/untab-region (&optional N)
+    (interactive "p")
+    (rofrol/indent-region (* (* (or N 1) tab-width)-1)))
+
+(defun  rofrol/tab-region (N)
+    (interactive "p")
+    (if (use-region-p)
+        (rofrol/indent-region (* (or N 1) tab-width)) ; region was selected, call indent-region
+        (rofrol/indent-lines N); else insert spaces as expected
+    ))
+
+(global-set-key (kbd "C->") 'rofrol/tab-region)
+(global-set-key (kbd "C-<") 'rofrol/untab-region)
