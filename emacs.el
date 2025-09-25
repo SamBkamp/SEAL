b594db2a4aeecec4647408f97c441c28432c6583
Sam Bonnekamp <sam@bonnekamp.net>
2025-09-25 15:29:19 +0800

    removed redudant code

--- emacs.el
+++ emacs.el
@@ -1,27 +1,3 @@
-(require 'package)
-(load-file "~/programming/SEAL/php-mode.el")
-
-(package-initialize)
-
-(add-to-list 'package-archives
-'("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deeper-blue)))
- '(package-selected-packages (quote (phps-mode magit-gitflow magit ##))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(magit-version)
 
 (defun bkamp/format-main-line()
   (kill-whole-line) 
